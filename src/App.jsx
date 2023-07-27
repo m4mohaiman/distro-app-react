504ea60243920970b89893bf4d046cd461f76942
batman <mohaiman.abdul@intellier.com>
2023-07-27 15:30:52 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='text-3xl font-bold underline text-orange-500'>
+      Hello world mohaiman
+      <button className='btn btn-info'>Info</button>
+    </h1>
+  )
+}
+
+export default App
